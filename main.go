0f50b50a4654b14a334a3ef848b86ed90994babc
matianqi <mtqmx3@gmail.com>
2021-12-12 21:38:45 +0800

    day 2

--- main.go
+++ main.go
@@ -6,24 +6,23 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
-}
+// Engine is the uni handler for all requests
+type Engine struct{}
 
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	_, err := fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-	if err != nil {
-		return
-	}
-}
-
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		_, err := fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		if err != nil {
-			return
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
+}
+
+func main() {
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
