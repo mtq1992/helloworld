beb2a276ba780d7225229cad23fe1407dd3a1276
matianqi <mtqmx3@gmail.com>
2021-12-12 22:00:10 +0800

    day 2

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module helloworld
 
 go 1.17
+
+require gee v0.0.0
+
+replace gee => ./gee
--- main.go
+++ main.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"gee"
 )
 
-// Engine is the uni handler for all requests
-type Engine struct{}
-
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/":
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-	case "/hello":
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
 		for k, v := range req.Header {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
-	default:
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
-}
+	})
 
-func main() {
-	engine := new(Engine)
-	log.Fatal(http.ListenAndServe(":9999", engine))
+	r.POST("/world", func(w http.ResponseWriter, req *http.Request) {
+
+		fmt.Fprintf(w, "Form Data = %q\n", len(req.Form))
+
+	})
+
+	r.Run(":9999")
 }
