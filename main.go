4eaa6c45de74434260d9be3f1acfca93d2064cbd
matianqi <mtqmx3@gmail.com>
2021-12-18 16:47:16 +0800

    group control

--- main.go
+++ main.go
@@ -11,6 +11,32 @@ func main() {
 		ctx.HTML(200, "<h1>hello world</h1>")
 	})
 
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(ctx *gee.Context) {
+			// expect /hello?name=geektutu
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(ctx *gee.Context) {
+			// expect /hello/geektutu
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
+		})
+		v2.POST("/login", func(ctx *gee.Context) {
+			ctx.JSON(http.StatusOK, gee.H{
+				"username": ctx.PostForm("username"),
+				"password": ctx.PostForm("password"),
+			})
+		})
+	}
+
 	r.GET("/hello", func(ctx *gee.Context) {
 		ctx.String(200, "hello %s, you at %s\n", ctx.Query("name"), ctx.Path)
 	})
